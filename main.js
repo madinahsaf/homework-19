8219fff5d74edf04e6c06cd2ddb3beaa632a36ba
madinahsaf <129226166+madinahsaf@users.noreply.github.com>
2023-05-17 22:55:18 +0100

    jquery changes

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log ("whats up");
+$(document).ready(function() {
+  var scrollLink = $('.scroll');
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+});
